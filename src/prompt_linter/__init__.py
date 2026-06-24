94e80debaf4854430058ceea8b1c7da4cdc2d78d
samuel <xiaoyyprocess@163.com>
2026-06-24 18:38:27 +0800

    feat: entropy-analyzer TDD 实现 (34/34 tests green)
    - 实现 EntropyAnalyzer 类
    - 注意力熵变率算法: layer[-1] - layer[-3] 熵差
    - 短文本安全回退机制
    - _classify 三档风险分类,

--- src/prompt_linter/__init__.py
+++ src/prompt_linter/__init__.py
@@ -1,5 +1,6 @@
 # Prompt 注意力诊断器
 
 from .model_loader import ModelLoader, ModelLoadError, OOMError
+from .entropy_analyzer import EntropyAnalyzer
 
-__all__ = ["ModelLoader", "ModelLoadError", "OOMError"]
+__all__ = ["ModelLoader", "ModelLoadError", "OOMError", "EntropyAnalyzer"]
